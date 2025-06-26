d1b476933c84a509462689d5735966be608aee8e
Adam Mrozek <mrozekss@gmail.com>
2025-06-26 20:34:12 +0200

    PaintBox

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,46 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import javax.swing.*
+import java.awt.*
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+// Custom JPanel that acts like a PaintBox
+class PaintBox : JPanel() {
+    private val points = mutableListOf<Point>()
+
+    init {
+        background = Color.WHITE
+        preferredSize = Dimension(400, 300)
+
+        // Mouse listener to draw points on mouse drag
+        addMouseMotionListener(object : MouseAdapter() {
+            override fun mouseDragged(e: MouseEvent) {
+                points.add(e.point)
+                repaint()
+            }
+        })
+    }
+
+    override fun paintComponent(g: Graphics) {
+        super.paintComponent(g)
+        val g2 = g as Graphics2D
+        g2.color = Color.BLACK
+        g2.stroke = BasicStroke(2f)
+        for (point in points) {
+            g2.fillOval(point.x, point.y, 4, 4)
+        }
+    }
+}
+
+fun main() {
+    SwingUtilities.invokeLater {
+        val frame = JFrame("PaintBox Example")
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+
+        val paintBox = PaintBox()
+        frame.contentPane.add(paintBox)
+
+        frame.pack()
+        frame.setLocationRelativeTo(null)
+        frame.isVisible = true
     }
 }
